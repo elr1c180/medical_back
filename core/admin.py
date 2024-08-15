f40d7d47d8bdb59c95af241708faca0f62833784
elr1c180 <elr1c.edw180@gmail.com>
2024-08-15 13:21:36 +0400

    try login

--- core/admin.py
+++ core/admin.py
@@ -1,18 +1,31 @@
 from django.contrib import admin
-from .models import Doctor, Patient, Medication, Procedure
+from .models import CustomUser, Doctor, Patient, Medication, Procedure
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'first_name', 'last_name', 'is_active', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
 
 @admin.register(Doctor)
 class DoctorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'middle_name', 'birth_date', 'phone_number')
+    list_display = ('user', 'birth_date', 'phone_number')
+    search_fields = ('user__email', 'user__last_name', 'user__first_name')
+    list_filter = ('birth_date',)
 
 @admin.register(Patient)
 class PatientAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'middle_name', 'phone_number', 'comment', 'doctor')
+    list_display = ('last_name', 'first_name', 'middle_name', 'phone_number', 'doctor')
+    search_fields = ('last_name', 'first_name', 'phone_number', 'doctor__user__email')
+    list_filter = ('doctor',)
 
 @admin.register(Medication)
 class MedicationAdmin(admin.ModelAdmin):
     list_display = ('name', 'quantity', 'doctor')
+    search_fields = ('name', 'doctor__user__email')
+    list_filter = ('doctor',)
 
 @admin.register(Procedure)
 class ProcedureAdmin(admin.ModelAdmin):
     list_display = ('name', 'date', 'patient', 'doctor')
+    search_fields = ('name', 'patient__last_name', 'doctor__user__email')
+    list_filter = ('doctor', 'date', 'patient')
