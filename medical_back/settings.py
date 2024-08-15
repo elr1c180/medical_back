50020f9b552fb7ce5d0a1e80f3222896f1e66ea8
elr1c180 <elr1c.edw180@gmail.com>
2024-08-15 14:59:37 +0400

    cart

--- medical_back/settings.py
+++ medical_back/settings.py
@@ -111,11 +111,8 @@ CORS_ALLOWED_ORIGINS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-       'rest_framework.authentication.TokenAuthentication',
-   ),
-   'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAdminUser'
-   ),
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
 }
 
 
